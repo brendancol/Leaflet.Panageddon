b69eb20caea610974770467ecdd1df94ac4475f3
Brendan Collins <bcollins@blueraster.com>
2014-07-20 16:26:38 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
 ##
 Sometimes you just want to pan 'til the break of day.  
 
-
--add bounding box property
--add bounce of walls of bounding box
 -add ability to start over after pan breaks bounding box
--add spiral path
 -add waypoint setting
 -add reverse path when hits bounding box edge
 -add reveal sample
 -add add bootstrap carousel sample
 -add golden spiral
--add upwind tacking pattern
+-add square spiral
+-add bounce wrap
+-add upwind tacking pattern (zig-zag w/ reflect)
+-add geojson and pan within continent of Africa example
+-add stop
+-add start horizontal and vertical alignment
+-add pan around feature border
+-fix panning speed when zooming in and out
+	-catch event, stop animation, start form last point w/scaled speed
+	
--- panageddon.js
+++ panageddon.js
@@ -14,7 +14,7 @@ L.PanageddonMap = L.Map.extend({
 		return this;
 	},
 
-	_bounce: function (options) {
+	addDefaults: function (options) {
 
 		if(!options.startingAngle) {
 			options.startingAngle = 90;
@@ -35,38 +35,100 @@ L.PanageddonMap = L.Map.extend({
 		if(!options.bbox) {
 			options.bbox = [-90, -180, 90, 180];
 		}
+	},
 
-		bounceOptions['bbox'];
+	_bounce: function (options) {
 
+		this.addDefaults(options);
 		var self = this;
 		var startingPosition = this.getCenter();
+		var startingZoom = this.getZoom();
+		var currentAngle = options.startingAngle;
 		var position = [startingPosition.lat, startingPosition.lng];
-		var dx = options.groundInterval * Math.cos(options.startingAngle * (180 / Math.PI));
-		var dy = options.groundInterval * Math.sin(options.startingAngle * (180 / Math.PI));
+		var dx = options.groundInterval * Math.cos(currentAngle * (180 / Math.PI));
+		var dy = options.groundInterval * Math.sin(currentAngle * (180 / Math.PI));
+		var dirtyY = false;
+		var dirtyX = false;
 		return function () {
 			console.log('bounce: ' + position.toString());
-			console.log('bbox' + options.bbox.toString());
-		 	position[0] = Number((position[0] + dy).toFixed(6));
-		 	position[1] = Number((position[1] + dx).toFixed(6));
+		 	dirtyY = position[0] < options.bbox[0] || position[0] > options.bbox[2];
+		 	dirtyX = position[1] < options.bbox[1] || position[1] > options.bbox[3];
+		 	
+		 	if (dirtyX || dirtyY) {
+		 		if (options.edgeMethod === 'reverse') {
+		 			dx = dx * -1;
+		 			dy = dy * -1;
+		 		} 
 
-		 	if (position[0] < options.bbox[0] || 
-		 		position[0] > options.bbox[2] ||
-		 		position[1] < options.bbox[1] || 
-		 		position[1] > options.bbox[3]) {
-		 		dx = dx * -1;
-		 		dy = dy * -1;
-		 		position[0] = Number((position[0] + dy).toFixed(6));
-		 		position[1] = Number((position[1] + dx).toFixed(6));
-		 		console.log('changing direction!!!');
+		 		if (dirtyX && options.edgeMethod === 'reflect') {
+		 			dx = dx * -1;
+		 		} 
 
-		 	}
+		 		if (dirtyY && options.edgeMethod === 'reflect') {
+		 			dy = dy * -1;
+		 		}
 
+		 		if (options.edgeMethod === 'repeat') {
+		 			position = [startingPosition.lat, startingPosition.lng];
+		 			self.setView(position, startingZoom, {
+		 				pan : {animate:false}
+		 			});
+		 			return;
+		 		}
+		 	} 
+
+	 		position[0] = Number((position[0] + dy).toFixed(6));
+	 		position[1] = Number((position[1] + dx).toFixed(6));
 			self.panTo(position, {animate: true, duration: options.duration, easeLinearity: 1.0});
 		}
 	},
 
 	bounce: function (options) {
 		var func = this._bounce(options);
+		this.activePathes.push(setInterval(func, options.refreshRate * 1000 + 100));
+	},
+
+	_spiral: function (options) {
+		this.addDefaults(options);
+		var self = this;
+		var startingPosition = this.getCenter();
+
+		var centerX = startingPosition.lng;
+		var radiusX = .05;
+		var dx = 0;
+		
+		var centerY = startingPosition.lat;
+		var radiusY = .05;
+		var dy = 0;
+
+
+		var dirtyY = false;
+		var dirtyX = false;
+		var i = 0;
+		var growthRate = 1;
+		var angle = 0;
+		return function () {
+			i = i + growthRate;
+			angle = 0.1 * i;
+	        dx = (radiusX + radiusY * angle) * Math.cos(angle);
+	        dy = (radiusX + radiusY * angle) * Math.sin(angle);
+
+		 	dirtyY = centerY + dy < options.bbox[0] || centerY + dy > options.bbox[2];
+		 	dirtyX = centerX + dx < options.bbox[1] || centerX + dx > options.bbox[3];
+		 	
+		 	if (dirtyX || dirtyY) {
+		 		growthRate = growthRate * -1;
+		 	} 
+
+	 		y = Number((centerY + dy).toFixed(6));
+	 		x = Number((centerX + dx).toFixed(6));
+	 		console.log('spiral: ' + y.toString() + x.toString());
+			self.panTo([y,x], {animate: true, duration: options.duration, easeLinearity: 1.0});
+		}
+	},
+
+	spiral: function (options) {
+		var func = this._spiral(options);
 		this.activePathes.push(setInterval(func, options.refreshRate * 1000 + 100));
 	}
 
